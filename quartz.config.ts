d44b151906c22871ce24e6d7d8621279a6d0b569
Giani Statie <gianistatie@gmail.com>
2023-10-18 20:50:19 +0300

    Quartz sync: Oct 18, 2023, 8:50 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "2BytesGoat",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "2bytesgoat.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
